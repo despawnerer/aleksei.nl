c689ed0d238c75c778a21aae1d8613a92637b315
Aleksei Voronov <despawn@gmail.com>
2021-09-21 21:41:21 +0200

    005

--- src/main.rs
+++ src/main.rs
@@ -3,196 +3,90 @@ use warp::filters;
 use warp::http::header::{CACHE_CONTROL, CONTENT_TYPE, REFRESH, SET_COOKIE};
 use warp::Filter;
 
-const REFRESH_TARGET: &str = "1;https://voronov.nl";
+const REFRESH_TARGET: &str = "2;https://voronov.nl";
 
 const IP: [u8; 4] = [0, 0, 0, 0];
 const PORT: u16 = 3030;
 
-const COOKIE_NAME: &str = "four";
-const MAX_CRITTERS: usize = 30;
+const COOKIE_NAME: &str = "five";
 
-struct Critter {
-    x: f32,
-    y: f32,
-}
-
-fn deserialize_critters(string: String) -> Vec<Critter> {
-    string
-        .split("|")
-        .filter_map(|critter_str| {
-            let mut values_str = critter_str.split(',');
-
-            let x = values_str.next().and_then(|x_str| x_str.parse().ok());
-            let y = values_str.next().and_then(|y_str| y_str.parse().ok());
-
-            let x = match x {
-                None => return None,
-                Some(x) => x,
-            };
-
-            let y = match y {
-                None => return None,
-                Some(y) => y,
-            };
-
-            if let Some(_) = values_str.next() {
-                return None;
-            }
-
-            Some(Critter { x, y })
-        })
-        .collect()
-}
-
-fn serialize_critters(critters: &Vec<Critter>) -> String {
-    let mut string = String::new();
-
-    let mut critters_iter = critters
-        .into_iter()
-        .map(|critter| format!("{},{}", critter.x, critter.y));
-
-    match critters_iter.next() {
-        Some(critter_str) => string.push_str(&critter_str),
-        None => return string,
-    };
-
-    for critter_str in critters_iter {
-        string.push('|');
-        string.push_str(&critter_str);
-    }
-
-    string
-}
+const GREETINGS: [&'static str; 12] = [
+    "Hi! I'm Mr. Voronov and this is my website!",
+    "Hoi! Ik ben meneer Voronov en dit is mijn website!",
+    "Привет! Я — г-н Воронов и это мой вебсайт!",
+    "¡Hola! ¡Soy el Sr. Voronov y este es mi sitio web!",
+    "أهلا! أنا السيد فورونوف وهذا موقع الويب الخاص بي!",
+    "Привіт! Я пан Воронов, і це мій веб-сайт!",
+    "<small>Help I am trapped in a website factory</small>",
+    "Salve! Dominus ego Voronov sum et hoc website meum est!",
+    "<small>Seriously, please help me</small>",
+    "Ciao! Sono il signor Voronov e questo è il mio sito web!",
+    "<small>This is not a joke, nobody knows I'm here</small>",
+    "Salut! Je suis M. Voronov et ceci est mon website!",
+];
 
 #[tokio::main]
 async fn main() {
     let hello =
         warp::any()
             .and(filters::cookie::optional(COOKIE_NAME))
-            .map(|serialized_critters: Option<String>| {
-                let mut rng = rand::thread_rng();
+            .map(|maybe_counter: Option<usize>| {
 
-                let mut critters = serialized_critters
-                    .map(deserialize_critters)
-                    .unwrap_or(Vec::new());
+                let mut greeting_number = maybe_counter.unwrap_or(0);
 
-                critters.push(Critter {
-                    x: rng.gen(),
-                    y: rng.gen(),
-                });
+                if greeting_number >= GREETINGS.len() {
+                    greeting_number = 0;
+                }
 
-                let mut html =
-                    r#"
+                let html =
+                    format!(r#"
 <title>Voronov</title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+
 <style>
-html, body {
+html, body {{
     padding: 0;
     margin: 0;
     width: 100%;
     height: 100%;
-}
+}}
 
-body {
-    background: white;
-    color: black;
-    white-space: nowrap;
+body {{
+    background: #8A2D1C;
+    color: white;
     overflow: hidden;
     text-align: center;
-}
+    font-size: 5vh;
+    font-family: 'PT Sans', sans-serif;
+    line-height: 5vh;
+}}
 
-.critter {
+div {{
     position: absolute;
-    padding: 20px;
-    border: 2px solid black;
-    border-radius: 15px;
-    background: white;
-}
-
-.eye {
-    width: 50px;
-    height: 50px;
-    border: 2px solid black;
-    border-radius: 50%;
-    overflow: hidden;
-    background: white;
-    display: inline-block;
-    position: relative;
-}
-
-.pupil {
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    position: absolute;
-    left: 15px;
-    top: 0;
-    background: black;
-}
-
-.mouth {
-    position: absolute;
-    bottom: 10px;
-    left: 50%;
-    font-family: sans-serif;
-    background: black;
-    border-radius: 50% 50% 0 0;
-    width: 30px;
-    height: 10px;
-    transform: translateX(-50%);
-}
-
-.voronov {
-    position: absolute;
-    left: 50%;
     top: 50%;
+    left: 50%;
     transform: translate(-50%, -50%);
-    font-size: 10vh;
-    text-align: center;
-}
+    max-width: 100%;
+}}
 
-.voronov small {
-    display: block;
-    font-size: 25%;
-}
+small {{
+    font-size: 50%;
+    opacity: 50%;
+}}
 </style>
 
-<script>
-let eyes = document.getElementsByClassName("eye");
 
-document.onmousemove = function(event) {
-    for (eye of eyes) {
-        let eyeX = eye.getBoundingClientRect().left;
-        let eyeY = eye.getBoundingClientRect().top;
-        let radianDegrees = Math.atan2(event.pageX - eyeX - 25, event.pageY - eyeY - 25);
-        let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
-        eye.style.transform = `rotate(${rotationDegrees}deg)`;
-    }
-};
-</script>
+<div>{}</div>
+"#, GREETINGS[greeting_number]);
 
-<div class="voronov">
-    (the) Voronov
-    <small>is watching you</small>
-</div>
-"#.to_string();
-
-                for critter in &critters {
-                    html.push_str(
-                        &format!(
-                            "<div class=\"critter\" style=\"left: {}%; top: {}%\"><div class=\"eye\"><div class=\"pupil\"></div></div> <div class=\"eye\"><div class=\"pupil\"></div></div><div class=\"mouth\"></div></div>",
-                            critter.x * 100.0,
-                            critter.y * 100.0
-                        )
-                    );
-                }
-
-                if critters.len() >= MAX_CRITTERS {
-                    critters = Vec::new();
-                }
+                greeting_number += 1;
 
                 warp::http::response::Builder::new()
                     .header(CONTENT_TYPE, "text/html; charset=utf-8")
-                    .header(SET_COOKIE, format!("{}={}", COOKIE_NAME, serialize_critters(&critters)))
+                    .header(SET_COOKIE, format!("{}={}", COOKIE_NAME, greeting_number.to_string()))
                     .header(CACHE_CONTROL, "no-store")
                     .header(REFRESH, REFRESH_TARGET)
                     .status(200)
